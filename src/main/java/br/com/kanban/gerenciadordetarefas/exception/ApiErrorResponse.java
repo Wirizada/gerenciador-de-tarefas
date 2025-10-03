65c7297c1111806b6a892bd7ced636f87525d995
Wirizada <ghostybrine@gmail.com>
2025-10-03 12:47:27 -0300

    feat(api): Formata o timestamp nas respostas de erro
    Adiciona a anotação `@JsonFormat` ao campo `timestamp` na classe `ApiErrorResponse`.
    
    Essa mudança formata a data e hora para um padrão ISO-8601 legível por humanos (ex: `2025-10-03T12:46:47-03:00`), em vez do formato numérico padrão (epoch timestamp). O objetivo é melhorar a legibilidade das respostas de erro, facilitando a depuração e a análise de logs para os desenvolvedores que consomem a API.

--- src/main/java/br/com/kanban/gerenciadordetarefas/exception/ApiErrorResponse.java
+++ src/main/java/br/com/kanban/gerenciadordetarefas/exception/ApiErrorResponse.java
@@ -1,9 +1,13 @@
 package br.com.kanban.gerenciadordetarefas.exception;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.LocalDateTime;
 
 public record ApiErrorResponse(
         int status,
         String message,
+
+        @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
         LocalDateTime timestamp
 ){}
